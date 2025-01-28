23e1ea2b3b4dc887177c9a8c9b0c06499c14ed52
neilmrtngamboa <nmg.gamboa@gmail.com>
2025-01-28 18:02:44 +0800

    Added weather details and changed the wind img

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import clearWeather from '../assets/Images/clear.png'
+import humidIcon from '../assets/Images/humid.png'
+import windIcon from '../assets/Images/wind.png'
+
 
 const Weather = () => {
     return (
@@ -11,6 +15,26 @@ const Weather = () => {
                             <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                         </svg>
                     </div>
+                    <img src={clearWeather} alt="clear" className='mt-10' style={{width:'15rem',height:'15rem'}} />
+                    <div className='text-center mt-5'>
+                        <p>16°C</p>
+                        <p>Tokyo</p>
+                    </div>
+
+                    <div className='grid grid-cols-2 mt-7'>
+
+                        <div className='flex gap-2'>
+                            <p className='my-auto'>22%</p>
+                            <img src={humidIcon} alt="humid" style={{width:'2.5rem', height:'2.5rem'}} />
+                        </div>
+
+                        <div className='flex gap-2'>
+                            <p className='my-auto'>2.8 Km/h</p>
+                            <img src={windIcon} alt="wind" style={{width:'2.5rem', height:'2.5rem'}} />
+                        </div>
+
+                    </div>
+                    
 
                 </div>
 
