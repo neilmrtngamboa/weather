65073a757fcce79ff1747fa8ba8ff8100f8d8222
neilmrtngamboa <nmg.gamboa@gmail.com>
2025-02-01 23:11:38 +0800

    added a then function to clear the input value after searching for a city

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -67,7 +67,7 @@ const Weather = () => {
                 <div className='bg-yellow-300 p-14 rounded-2xl shadow-2xl shadow-yellow-500 mt-16'>
                     <div className='flex gap-2'>
                         <input type="text" ref={weatherRef} placeholder='Search' className='p-1.5 shadow-md border border-blue-800 rounded' />
-                        <button className='flex bg-yellow-300 px-5 shadow-lg hover:bg-yellow-400 hover:shadow-none' onClick={() => findCity(weatherRef.current.value)}>
+                        <button className='flex bg-yellow-300 px-5 shadow-lg hover:bg-yellow-400 hover:shadow-none' onClick={() => findCity(weatherRef.current.value).then(weatherRef.current.value = '')}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 my-auto">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                             </svg>
